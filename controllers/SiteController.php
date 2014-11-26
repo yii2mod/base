247c1829129d5154a490cc0ef74dfa6d70a18caa
Chepurnoy <igorzfort@gmail.com>
2014-11-26 20:22:33 +0200

    add contact us

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,9 +2,11 @@
 
 namespace app\controllers;
 
+use app\models\forms\ContactForm;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
+use Yii;
 
 /**
  * Class SiteController
@@ -39,6 +41,7 @@ class SiteController extends Controller
     }
 
     /**
+     * Actions
      * @return array
      */
     public function actions()
@@ -47,15 +50,37 @@ class SiteController extends Controller
             'error' => [
                 'class' => 'yii\web\ErrorAction',
             ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
         ];
     }
 
     /**
+     * Index action
      * @return string
      */
     public function actionIndex()
     {
         return $this->render('index');
+    }
+
+    /**
+     * Contact us action
+     * @return string|\yii\web\Response
+     */
+    public function actionContact()
+    {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+            Yii::$app->session->setFlash('contactFormSubmitted');
+            return $this->refresh();
+        } else {
+            return $this->render('contact', [
+                'model' => $model,
+            ]);
+        }
     }
 
 }
