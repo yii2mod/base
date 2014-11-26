d834b53be724185056a991e4686f5397a75b03a4
igor <igorzfort@gmail.com>
2014-11-26 19:50:43 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,6 +6,11 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/../config/web.php');
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../config/main.php'),
+    require(__DIR__ . '/../config/main.local.php'),
+    require(__DIR__ . '/../config/common.php'),
+    require(__DIR__ . '/../config/common.local.php')
+);
 
 (new yii\web\Application($config))->run();
