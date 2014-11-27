02fea47962adfc554b51fc24aa87c5b15c8575df
Chepurnoy <igorzfort@gmail.com>
2014-11-27 12:46:28 +0200

    add password reset main template

---
+++ mail/passwordResetToken.php
@@ -0,0 +1,14 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $user common\models\User */
+
+$resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/password-reset', 'token' => $user->passwordResetToken]);
+?>
+
+Hello <?php echo Html::encode($user->username) ?>,
+
+Follow the link below to reset your password:
+
+<?php echo Html::a(Html::encode($resetLink), $resetLink) ?>
