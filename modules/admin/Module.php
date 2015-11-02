40ed0f1605e9d46f2dfc52e00b245473aa10f0af
Igor Chepurnoy <igorzfort@gmail.com>
2015-11-02 11:48:44 +0200

    Update Module.php

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -29,7 +29,7 @@ class Module extends \yii\base\Module
     public $controllerNamespace = 'app\modules\admin\controllers';
 
     /**
-     *
+     * Init module
      */
     public function init()
     {
