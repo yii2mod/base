fb40fb06656dd12a688124d02fe73fae427b22d9
Igor Chepurnoy <igorzfort@gmail.com>
2016-02-24 14:29:14 +0200

    Update SignupCept.php

--- tests/codeception/acceptance/SignupCept.php
+++ tests/codeception/acceptance/SignupCept.php
@@ -3,63 +3,30 @@ use codeception\_pages\SignupPage;
 
 $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that signup works');
-
-
 $signupPage = SignupPage::openBy($I);
-
-$I->see('New Customer Signup', 'h1');
-
-$I->amGoingTo('try to sign up with empty fields');
+$I->see('Signup', 'h1');
+$I->see('Please fill out the following fields to signup:');
+$I->amGoingTo('submit signup form with no data');
 $signupPage->submit([]);
-$I->wait(1);
-$I->expectTo('see validations errors');
-$I->see('Customer Name cannot be blank.');
-$I->see('Confirm Password cannot be blank.');
-$I->see('Email Address cannot be blank.');
-
-$I->amGoingTo('try to signup with existing email');
-
+$I->expectTo('see validation errors');
+$I->see('Username cannot be blank.', '.help-block');
+$I->see('Email cannot be blank.', '.help-block');
+$I->see('Password cannot be blank.', '.help-block');
+$I->amGoingTo('submit signup form with not correct email');
 $signupPage->submit([
-    "username" => "Dmitry",
-    "license" => "12344",
-    "password" => "123123",
-    "confirmPassword" => "123123",
-    "addressName" => "heroiv",
-    "address1" => "sta",
-    "address2" => "ta",
-    "address1" => "sta123",
-    "address2" => "ta345",
-    "city" => "Kharkiv",
-    "stateId" => "District of Columbia",
-    "zip" => "456",
-    "phone" => "123123123",
-    "fax" => "123123123",
-    "firstName" => "qweqwr",
-    "lastName" => "rtyrty",
-    "title" => "345456",
-    "phone1" => "123123123",
-    "phone1Ext" => "33",
-    "phone1Location" => "Home",
-    "phone1Location" => "2",
-    "phone2" => "wewert",
-    "phone2Ext" => "546",
-    "phone2Location" => "Home",
-    "phone2Location" => "2",
-    "phone2" => "456456456",
-    "email" => "disemx@gmail.com",
-    "hours" => "233",
+    'username' => 'tester',
+    'email' => 'tester.email',
+    'password' => 'tester_password',
 ]);
-
-
-$I->wait(1);
-
-
-$I->expectTo('see user info');
-$I->see('Logout');
-
-
-
-
-
-
-
+$I->expectTo('see that email address is wrong');
+$I->dontSee('Username cannot be blank.', '.help-block');
+$I->dontSee('Password cannot be blank.', '.help-block');
+$I->see('Email is not a valid email address.', '.help-block');
+$I->amGoingTo('submit signup form with correct email');
+$signupPage->submit([
+    'username' => 'tester',
+    'email' => 'tester.email@example.com',
+    'password' => 'tester_password',
+]);
+$I->expectTo('see that user logged in');
+$I->see('Logout (tester)', 'form button[type=submit]');
