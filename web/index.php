7c349da2e0ab994cd154b7b081232a87cd86cc48
vals2004 <vals2004@gmail.com>
2016-04-14 13:01:16 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,10 +7,10 @@ require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = yii\helpers\ArrayHelper::merge(
-    require(__DIR__ . '/../config/main.php'),
-    require(__DIR__ . '/../config/main.local.php'),
     require(__DIR__ . '/../config/common.php'),
-    require(__DIR__ . '/../config/common.local.php')
+    require(__DIR__ . '/../config/common.local.php'),
+    require(__DIR__ . '/../config/main.php'),
+    require(__DIR__ . '/../config/main.local.php')
 );
 
 (new yii\web\Application($config))->run();
