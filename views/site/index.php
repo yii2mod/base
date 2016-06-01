70ddbd54e0e8ae8307f0517bd248b726cd4dd83c
Igor Chepurnoy <igorzfort@gmail.com>
2016-06-01 13:59:07 +0300

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -3,12 +3,8 @@
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
-
     <div class="jumbotron">
         <h1>Congratulations!</h1>
-
         <p class="lead">You have successfully created your Yii-powered application.</p>
     </div>
-
-
 </div>
