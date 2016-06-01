b7e9412247c0d773d0b316b354f3b2bc31987b33
Igor Chepurnoy <igorzfort@gmail.com>
2016-06-01 18:42:26 +0300

    add schedule.php

---
+++ config/schedule.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @var \yii2mod\scheduling\Schedule $schedule
+ *
+ * Add to cron the following command:
+ *
+ *  ~~~
+ *     * * * * * php yii schedule/run
+ *  ~~~
+ *
+ * Also you can set the schedule file path:
+ *
+ * ~~~
+ *     * * * * * php yii schedule/run --scheduleFile=@app/config/schedule.php
+ * ~~~
+ *
+ *
+ * @see https://github.com/yii2mod/yii2-scheduling
+ */
+// for example: generate sitemap every week
+$schedule->command('app/generate-sitemap')->description('Generate Sitemap')->weekly();
+
