b49f2645328475ab4f465b1bec5c3880e251e298
Igor Chepurnoy <igorzfort@gmail.com>
2016-08-03 20:05:49 +0300

    update migration for create Setting table

--- migrations/m130524_201442_init.php
+++ migrations/m130524_201442_init.php
@@ -109,7 +109,7 @@ class m130524_201442_init extends Migration
             'section' => $this->string()->notNull(),
             'key' => $this->string()->notNull(),
             'value' => $this->text()->notNull(),
-            'status' => $this->boolean(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(1),
             'createdAt' => $this->integer()->notNull(),
             'updatedAt' => $this->integer()->notNull()
         ], $this->tableOptions);
