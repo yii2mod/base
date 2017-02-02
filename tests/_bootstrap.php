7c77442ee985c2b7bb5c6c38e2c939318873c628
Igor Chepurnoy <igorzfort@gmail.com>
2017-02-02 10:26:42 +0200

    Update _bootstrap.php

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -5,5 +5,3 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 require __DIR__ . '/../vendor/autoload.php';
-
-Yii::setAlias('@codecept_data_dir', __DIR__ . '/_data');
